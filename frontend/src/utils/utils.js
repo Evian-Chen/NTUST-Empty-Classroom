bd91ab5f56df8a1c12e810a829ed0c883cb34b8d
Evian-Chen <paramecium128@gmail.com>
2025-10-18 10:58:28 +0800

    update getlastsat function and workflow

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,8 +1,7 @@
 export const getLastSat = () => {
   const today = new Date();
   const dayOfWeek = today.getDay(); // 0 = Sunday, 1 = Monday, ..., 6 = Saturday
-  const daysUntilLastSaturday = dayOfWeek === 0 ? 1 : dayOfWeek + 1; // Days to go back to last Saturday
-
+  const daysUntilLastSaturday = dayOfWeek === 6 ? 0 : dayOfWeek + 2; // Days to go back to last Saturday
   const lastSaturday = new Date(today);
   lastSaturday.setDate(today.getDate() - daysUntilLastSaturday);
   return lastSaturday;
